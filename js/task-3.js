13429a989e8e3b3973c35dbbdbbfdb6c7ffdb06f
Vladislav Rebrov <rebrovvlad@gmail.com>
2025-02-06 14:56:24 +0200

    markup_2

--- js/task-3.js
+++ js/task-3.js
@@ -39,7 +39,7 @@ builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
 */
 
-
+/*
 class StringBuilder {
   constructor(initialValue) {
     this._value = initialValue; // Приватна властивість
@@ -63,6 +63,49 @@ class StringBuilder {
   // Метод для додавання рядка на початок і в кінець
   padBoth(str) {
     this._value = str + this._value + str;
+  }
+}
+
+// Перевірка роботи класу
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+*/
+
+
+class StringBuilder {
+  #value; // Оголошуємо приватну властивість
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  // Метод для отримання значення приватної властивості
+  getValue() {
+    return this.#value;
+  }
+
+  // Метод для додавання рядка в кінець
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  // Метод для додавання рядка на початок
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  // Метод для додавання рядка на початок і в кінець
+  padBoth(str) {
+    this.#value = str + this.#value + str;
   }
 }
 
